7ae0899e73b068a58592da0b5e50455620a2e698
iamayo <iamjerrydev@gmail.com>
2022-03-14 09:30:13 -0700

    The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,7 +4,7 @@
 /**
  * main - entry point
  * Description: prints the last digit of the stored variable
- * Return: Always 0 (success) 
+ * Return:Always 0 (success) 
  */
 
 /* betty style doc for function main goes there */
@@ -20,14 +20,13 @@ if (lastdigit > 5)
 {
 printf("Last digit of %d is %d and is greater than 5\n", n, lastdigit);
 }
-else if (lastdigit < 6 && lastdigit !=0)
+else if (lastdigit < 6 && lastdigit ! = 0)
 {
-  printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdigit);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdigit);
 }
-else if (lastdigit ==0)
+else if (lastdigit == 0)
 {
 printf("Last digit of %d is %d and is 0\n", n, lastdigit);
-}  
+}
 return (0);
 }
-
