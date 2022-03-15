1ad26ab4eec5f880d3670df459c0114f59a839af
iamayo <iamjerrydev@gmail.com>
2022-03-15 11:13:47 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,6 +3,7 @@
 /**
  * _islower - Entry point
  * Description: A function that checks for lowercase character
+ * @c: the integer value it receives
  * Return: 1 if TRUE 0 if FALSE
  */
 int _islower(int c)
