e0ba837991b487979f5edbca63adb908ef790d29
iamayo <iamjerrydev@gmail.com>
2022-03-15 10:05:05 -0700

    functions

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ * Description: A program that prints all possible combinations of two two-digit numbers.
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet - Entry point
+ * Description: A function that prints the alphabet, in lowercase
+ * Return: void
+ */
+void print_alphabet(void)
+{
+char alphabet = 'a';
+
+while (alphabet <= 'z')
+{
+_putchar(alphabet);
+letter++;
+}
+_putchar('\n');
+}
+
+
