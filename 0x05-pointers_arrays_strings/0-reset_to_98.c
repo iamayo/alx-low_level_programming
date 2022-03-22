792f8297891ca5fd5ef476657a3bd5fa42e223ff
iamayo <iamjerrydev@gmail.com>
2022-03-22 09:26:24 -0700

    Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -9,16 +9,16 @@
  */
 int main(void)
 {
-    void reset_to_98(int *n);
-    int n;
+void reset_to_98(int *n);
+int n;
 
-    n = 402;
-    printf("n=%d\n", n);
-    reset_to_98(&n);
-    printf("n=%d\n", n);
-    return (0);
+n = 402;
+printf("n=%d\n", n);
+reset_to_98(&n);
+printf("n=%d\n", n);
+return (0);
 }
 void reset_to_98(int *n)
 {
-	*n = 98;
+*n = 98;
 }
