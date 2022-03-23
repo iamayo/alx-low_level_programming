fb593202982d57ba5b5bebb8a82fa9cd1595a30f
iamayo <iamjerrydev@gmail.com>
2022-03-23 08:40:58 -0700

    strrev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: the used string reference pointer
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+int i = 0;
+
+while (s[i])
+i++;
+while (i--)
+{
+putchar(s[i]);
+}
+putchar('\n');
+}
