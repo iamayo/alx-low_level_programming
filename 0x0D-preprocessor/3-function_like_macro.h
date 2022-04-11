96ee1f8e3d5b1a69e75872767fc17577ab7bf580
iamayo <iamjerrydev@gmail.com>
2022-04-11 14:25:51 -0700

    c - Preprocessor

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,13 @@
+#ifndef function_like_macro_h
+#define function_like_macro_h
+
+/**
+ * File: 3-function_like_macro.h
+ * Desc: a function-like macro ABS(x)
+ * that computes the absolute value of a number x.
+ * @x: ABS(x)
+ */
+
+#define ABS(x) ((x) < 0 ? (-x) : (x))
+
+#endif /* ABS(x) */
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,14 @@
+#ifndef sum_h
+#define sum_h
+
+/**
+ * File: 4-sum.h
+ * Desc: a function-like macro SUM(x, y)
+ * that computes the sum of the numbers x and y.
+ * @x: SUM(x)
+ * @y: SUM(y)
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
