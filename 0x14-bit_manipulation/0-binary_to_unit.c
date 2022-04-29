1ee750f74cd704832cee925047cb3923b3565cd1
iamayo <iamjerrydev@gmail.com>
2022-04-28 18:36:47 -0700

    binary

--- 0x14-bit_manipulation/0-binary_to_unit.c
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -10,7 +9,7 @@
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
-	unsigned int dec = 0;
+	unsigned int decimal = 0;
 
 	if (!b)
 		return (0);
@@ -19,8 +18,8 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		dec = 2 * dec + (b[i] - '0');
+		decimal = 2 * decimal + (b[i] - '0');
 	}
 
-	return (dec);
+	return (decimal);
 }
